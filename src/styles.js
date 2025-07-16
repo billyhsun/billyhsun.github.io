d50a264d05960174274d8805b59df74ebd2c3309
Bill Sun <ysun@kinaxis.com>
2025-07-15 23:00:04 -0400

    formatting

--- src/styles.js
+++ src/styles.js
@@ -28,7 +28,6 @@ class Head extends HTMLElement {
             <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
             <link rel="stylesheet" href="src/styles.css">
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-            <link rel="icon" type="image/png" href="logos/b.png" sizes="32x32">
         </head>
 
         `
