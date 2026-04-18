c86e38bcb43b4522ece931693a3a9352c013fbdd
Bill Sun <bsun@guidepoint.com>
2026-04-18 10:38:17 -0400

    footer

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -118,16 +118,15 @@ class Footer extends HTMLElement {
         `
 		<div class="footer">
 			<p style="margin: 0; padding: 0;">
-				<font color=#cccccc>Copyright 2018-2025 © Bill Yuan Hong Sun</font>
+				<font color=#cccccc>Copyright 2018-2026 © Bill Yuan Hong Sun</font>
 			</p>
 			<div class="socialmedia">
                 <a href="https://www.linkedin.com/in/bill-yuan-hong-sun/"><i class="fa-brands fa-linkedin-in" aria-hidden="true"></i></a>
 				<a href="https://github.com/billyhsun"><i class="fa-brands fa-github" aria-hidden="true"></i></a>
                 <a href="https://www.facebook.com/billyhsun"><i class="fa-brands fa-facebook" aria-hidden="true"></i></a>
 				<a href="https://www.instagram.com/billyhsun/"><i class="fa-brands fa-instagram" aria-hidden="true"></i></a>
+                <a href="https://www.threads.com/@billyhsun"><i class="fa-brands fa-threads" aria-hidden="true"></i></a>
                 <a href="https://x.com/billyhsun"><i class="fa-brands fa-x-twitter" aria-hidden="true"></i></a>
-                <a href="https://www.researchgate.net/profile/Yuan-Hong-Sun"><i class="fa-brands fa-researchgate"></i></a>
-                <a href="https://calendly.com/billyhsun/30min"><i class="fa fa-calendar" aria-hidden="true"></i></a>
 				<a href="mailto:billyuanhong.sun@mail.utoronto.ca"><i class="fas fa-envelope" aria-hidden="true"></i></a>
                 <a href="https://linktr.ee/billyhsun"><i class="fa-solid fa-link" aria-hidden="true"></i></a>
 			</div>
